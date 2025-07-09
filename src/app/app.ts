edf85f4e4fca88a91fe03ad1dbbcfdead6bf7495
vieerr <olivier_paspuel@hotmail.com>
2025-07-09 09:18:00 -0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'Paspuel_Olivier_Ex2_Contador';
+}
